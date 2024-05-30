d20dc66100797dc1a932b12bd5dbea71f5401074
thehsadowte <thehsadowte@gmail.com>
2024-05-30 18:28:36 +0300

    dashboard component

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,38 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+// const getSalesData = () => {}
+
 export default function AdminDashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard title="Sales" subtitle="description" body="text" />
+      <DashboardCard title="Something" subtitle="description" body="text" />
+    </div>
+  );
+}
+
+type DashboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import { cn } from '@/lib/utils';
-import { Link } from 'next/link';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React, { ComponentProps, ReactNode } from 'react';
 
@@ -13,8 +15,15 @@ export function Nav({ children }: { children: ReactNode }) {
   );
 }
 
-export function NavLink(props: Omit<ComponentProps<typeof Link>, "className") {
-    const pathname = usePathname()
-    return <Link{...props} className={cn("p-4 hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground", pathname === props.href && "bg-background text-foreground"
-  )} />;
+export function NavLink(props: Omit<ComponentProps<typeof Link>, 'className'>) {
+  const pathname = usePathname();
+  return (
+    <Link
+      {...props}
+      className={cn(
+        'p-4 hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground',
+        pathname === props.href && 'bg-background text-foreground'
+      )}
+    />
+  );
 }
