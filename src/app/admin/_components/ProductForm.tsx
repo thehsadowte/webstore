7d5daf2fb497124ae0423ada068aa9c7fc188f7a
thehsadowte <thehsadowte@gmail.com>
2024-06-07 00:12:30 +0300

    add product admin

--- src/app/admin/_components/ProductForm.tsx
+++ src/app/admin/_components/ProductForm.tsx
@@ -1,15 +1,46 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { formatCurrency } from '@/lib/formatters';
+import { useState } from 'react';
 
 export default function ProductForm() {
+  const [priceInCents, setPriceInCents] = useState<number>();
   return (
-    <form>
-      <div>
+    <form className="space-y-8">
+      <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="text" name="name" required></Input>
+        <Input type="text" id="text" name="name" required />
       </div>
+      <div className="space-y-2">
+        <Label htmlFor="priceInCents">Price in Cents</Label>
+        <Input
+          type="text"
+          id="priceInCents"
+          name="priceInCents"
+          required
+          value={priceInCents}
+          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
+        />
+        <div className="text-muted-foreground">
+          {formatCurrency((priceInCents || 0) / 100)}
+        </div>
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="description">Description</Label>
+        <Input type="text" id="description" name="description" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="file">File</Label>
+        <Input type="file" id="file" name="file" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="image">Image</Label>
+        <Input type="image" id="image" name="image" required />
+      </div>
+      <Button type="submit">Save</Button>
     </form>
   );
 }
