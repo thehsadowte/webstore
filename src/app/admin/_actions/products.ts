4f46d6e6f31f787417d976726fe2ae42f3e2909d
thehsadowte <thehsadowte@gmail.com>
2024-06-08 00:13:52 +0300

    dropdown-menu with problems

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -19,7 +19,10 @@ const addSchema = z.object({
   image: imageSchema.refine((file) => file.size > 0, 'Required'),
 });
 
-export default async function addProduct(formData: FormData) {
+export default async function addProduct(
+  prevState: unknown,
+  formData: FormData
+) {
   const result = addSchema.safeParse(Object.fromEntries(formData.entries()));
 
   if (result.success === false) {
@@ -41,6 +44,7 @@ export default async function addProduct(formData: FormData) {
 
   db.product.create({
     data: {
+      isAvailableForPurchase: false,
       name: data.name,
       description: data.description,
       priceInCents: data.priceInCents,
@@ -50,4 +54,20 @@ export default async function addProduct(formData: FormData) {
   });
 
   redirect('/admin/products');
+}
+
+export async function toggleProductAvailability(
+  id: string,
+  isAvailableForPurchase: boolean
+) {
+  await db.product.update({ where: { id }, data: { isAvailableForPurchase } });
+}
+
+export async function deleteProduct(id: string) {
+  const product = await db.product.delete({ where: { id } });
+
+  if (product == null) return notFound();
+
+  await fs.unlink(product.filePath);
+  await fs.unlink(`public${product.imagePath}`);
 }
--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -5,14 +5,24 @@ import { Button } from '@/components/ui/button';
 import {
   Table,
   TableBody,
+  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
+import { CheckCircle2, MoreVertical, XCircle } from 'lucide-react';
+import { formatCurrency, formatNumber } from '@/lib/formatters';
+import db from '@/db/db';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 // export interface IAppProps {}
 
-export default function Products(props: IAppProps) {
+export default function Products() {
   return (
     <>
       <div className="flex justify-between items-center gap-4">
@@ -26,7 +36,21 @@ export default function Products(props: IAppProps) {
   );
 }
 
-const ProductsTable = () => {
+const ProductsTable = async () => {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      priceInCents: true,
+      isAvailableForPurchase: true,
+      _count: { select: { orders: true } },
+    },
+    orderBy: { name: 'asc' },
+  });
+
+  if (products.length === 0)
+    return <p className="text-destructive">No products found ...</p>;
+
   return (
     <Table>
       <TableHeader>
@@ -42,7 +66,50 @@ const ProductsTable = () => {
           </TableHead>
         </TableRow>
       </TableHeader>
-      <TableBody></TableBody>
+      <TableBody>
+        {products.map((product) => (
+          <TableRow key={product.id}>
+            <TableCell>
+              {product.isAvailableForPurchase ? (
+                <>
+                  <CheckCircle2 />
+                  <span className="sr-only">Available</span>
+                </>
+              ) : (
+                <>
+                  <span className="sr-only">Unavailable</span>
+
+                  <XCircle></XCircle>
+                </>
+              )}
+            </TableCell>
+            <TableCell>{product.name}</TableCell>
+            <TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
+            <TableCell>{formatNumber(product._count.orders)}</TableCell>
+            <TableCell>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <MoreVertical />
+                  <span className="sr-only">Actions</span>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent>
+                  <DropdownMenuItem asChild>
+                    <a download={`/admin/products/${product.id}/download`}>
+                      Download
+                    </a>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <Link href={`/admin/products/${product.id}/edit`}>
+                      Edit
+                    </Link>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
     </Table>
   );
 };
